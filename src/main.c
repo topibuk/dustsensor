e66ac634eeb7b60ec8a153b17560fa259e510ff2
Victor Abramov <vicabramov@gmail.com>
2020-05-06 20:37:53 +0300

    initial implementation of wifi

--- src/main.c
+++ src/main.c
@@ -3,9 +3,14 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
+#include "freertos/event_groups.h"
 #include "driver/uart.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "secrets.h"
 
 #define PIN_RX GPIO_NUM_16
 #define PIN_TX GPIO_NUM_17
@@ -15,6 +20,8 @@
 #define LOG_TAG "main"
 
 #define BUF_SIZE UART_FIFO_LEN * 2
+
+#define WIFI_CONNECT_MAXIMUM_RETRY 100
 
 typedef struct DustSens_data
 {
@@ -35,6 +42,18 @@ typedef struct DustSens_data
 DustSens_t dust_mesurement;
 
 SemaphoreHandle_t dust_semaphore;
+
+uint8_t wifi_connect_retry_counter;
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* 
+- connected to the AP with an IP
+- maximum number of retries reached
+ */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
 
 void parse_dust_data(const uint8_t *data, uint8_t length)
 {
@@ -131,10 +150,111 @@ static void dust_sensor_task()
     }
 }
 
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT)
+    {
+        if (event_id == WIFI_EVENT_STA_START)
+        {
+            esp_wifi_connect();
+            wifi_connect_retry_counter++;
+            ESP_LOGI(LOG_TAG, "connecting to the AP");
+        }
+        else if (event_id == WIFI_EVENT_STA_DISCONNECTED)
+        {
+            if (wifi_connect_retry_counter < WIFI_CONNECT_MAXIMUM_RETRY)
+            {
+                esp_wifi_connect();
+                wifi_connect_retry_counter++;
+                ESP_LOGI(LOG_TAG, "connecting to the AP");
+            }
+            else
+            {
+                xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+            }
+            ESP_LOGI(LOG_TAG, "connect to the AP fail");
+        }
+    }
+    else if (event_base == IP_EVENT)
+    {
+        if (event_id == IP_EVENT_STA_GOT_IP)
+        {
+            ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+            ESP_LOGI(LOG_TAG, "got ip:%s",
+                     ip4addr_ntoa(&event->ip_info.ip));
+            wifi_connect_retry_counter = 0;
+            xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+        }
+    }
+}
+
+void start_wifi()
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    tcpip_adapter_init();
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD},
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(LOG_TAG, "wifi in station mode started");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT)
+    {
+        ESP_LOGI(LOG_TAG, "connected to ap SSID:%s", WIFI_SSID);
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
+        ESP_LOGI(LOG_TAG, "Failed to connect to SSID:%s", WIFI_SSID);
+    }
+    else
+    {
+        ESP_LOGE(LOG_TAG, "UNEXPECTED EVENT");
+    }
+}
+
 void app_main()
 {
     dust_semaphore = xSemaphoreCreateBinary();
     xSemaphoreGive(dust_semaphore);
 
-    xTaskCreate(dust_sensor_task, "dust_sensor_task", 1024, NULL, 10, NULL);
+    //Initialize NVS for internal use of WiFi
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(LOG_TAG, "starting WiFi in station mode");
+
+    start_wifi();
+
+    //    xTaskCreate(dust_sensor_task, "dust_sensor_task", 1024, NULL, 10, NULL);
 }

fbed86afa9876061e9cd9c509b5fd4f067ae75a3
Victor Abramov <vicabramov@gmail.com>
2020-05-11 16:31:38 +0300

    co2 sensor implementation added

--- src/main.c
+++ src/main.c
@@ -260,6 +260,87 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     return;
 }
 
+void publish_co2_data(const uint8_t *data, uint8_t length)
+{
+    uint16_t co2_ppm;
+
+    if (length != 9)
+    {
+        return;
+    }
+    co2_ppm = (uint16_t)((uint16_t)data[2] << 8 | (uint16_t)data[3]);
+
+    ESP_LOGD(LOG_TAG, "read co2 ppm is %i", co2_ppm);
+}
+
+static void co2_sensor_task()
+{
+    /*
+    This is a mh-z19b procedure
+    */
+
+    uart_config_t in_config = {
+
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+
+    };
+
+    uart_param_config(UART_NUM_1, &in_config);
+    uart_set_pin(UART_NUM_1, CO2_PIN_TX, CO2_PIN_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_driver_install(UART_NUM_1, BUF_SIZE, BUF_SIZE, 0, NULL, 0);
+
+    char cmd_read[] = {
+        0xFF,
+        0x01,
+        0x86,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x79,
+    };
+
+    for (;;)
+    {
+
+        int8_t res;
+        uint8_t data[9];
+        uint8_t count = 0;
+        uint8_t fails_count = 0;
+
+        res = uart_write_bytes(UART_NUM_1, (const char *)cmd_read, sizeof(cmd_read));
+
+        if (res < 0)
+        {
+            ESP_LOGE(LOG_TAG, "can't write to co2 sensor, panic");
+            return;
+        }
+
+        while (count < sizeof(data))
+        {
+            count = uart_read_bytes(UART_NUM_1, data + count, sizeof(data) - count, 200 / portTICK_PERIOD_MS);
+            if (count <= 0)
+            {
+                fails_count++;
+            }
+
+            if (fails_count > CO2_READ_FAILS)
+            {
+                ESP_LOGW(LOG_TAG, "unable to read from co2 sensor");
+                break;
+            }
+
+            publish_co2_data(data, count);
+            vTaskDelay(CO2_TASK_DELAY / portTICK_PERIOD_MS);
+        }
+    }
+}
+
 void start_mqtt_client()
 {
     esp_mqtt_client_config_t mqtt_cfg = {
